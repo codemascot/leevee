0b8fe199ad0c8a4b00a89e95fca8681369a34b1b
Khan Mohammad R <codemascot@hotmail.com>
2020-12-29 10:14:41 +0600

    Deps added to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
 (defproject leevee "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A todo list CLI app based on Ivy Lee method, written in Clojure."
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+  :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.cli "1.0.194"]
+                 [org.clojure/java.jdbc "0.7.5"]
+                 [environ "1.2.0"]
+                 [org.xerial/sqlite-jdbc "3.34.0"]
+                 [mysql/mysql-connector-java "8.0.22"]]
+  :plugins [[lein-environ "1.2.0"]]
   :repl-options {:init-ns leevee.core})
