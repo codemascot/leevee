566671838411a4e9c2005c03c79bd44085926822
Khan Mohammad R <codemascot@hotmail.com>
2020-12-29 16:16:22 +0600

    core functions added

--- src/leevee/core.clj
+++ src/leevee/core.clj
@@ -1,6 +1,65 @@
-(ns leevee.core)
+(ns leevee.core
+  ""
+  (:require [leevee.cli :as cli]
+            [leevee.db :as db]
+            [leevee.util :as util])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def tasks (atom {:date ""
+                  :tasks {}}))
+
+(defn tasks-input
+  ""
+  [^Integer number, ^Integer chars, ^String date]
+  (swap! tasks assoc :date date)
+  (dotimes [i number]
+    (println "Task No." (inc i))
+    (do (print (str "> "))
+        (flush)
+        (swap! tasks assoc-in [:tasks (inc i)] (util/trim-text chars (read-line))))))
+
+(defn add-tasks
+  ""
+  [options]
+  (let [number 3
+        chars 80
+        today (.format (java.text.SimpleDateFormat. "yyyy-M-dd") (new java.util.Date))
+        date (if (nil? (:day options)) today (:day options))]
+    (if (db/exists? "task_dates" "task_date" date)
+      (cli/exit 1 "Tasks already exist for this date!")
+      (tasks-input number chars date)))
+  (db/insert-tasks @tasks)
+  ;; TODO: Need to make this msg conditional on insert-tasks
+  (cli/exit 0 "\nThe tasks added successfully."))
+
+(defn list-tasks
+  ""
+  [options]
+  (let [today (.format (java.text.SimpleDateFormat. "yyyy-M-dd") (new java.util.Date))
+        date (if (nil? (:day options)) today (:day options))
+        result (db/retrive-tasks date)]
+    (println "\nTask list for" date ":\n")
+    (doseq [i (into [] (db/retrive-tasks date))] (println (str "[" (:o i) "] " "[" (:s i) "]") (:t i) (str "(ID:" (:i i) ")") ))))
+
+(defn edit-tasks
+  ""
+  [options]
+  (println "edit " options)
+  )
+
+(defn delete-tasks
+  ""
+  [options]
+  (println "delete " options)
+  )
+
+(defn -main [& args]
+  (let [{:keys [action options exit-message ok?]} (cli/validate-args args)]
+    (println (util/ascii-art))
+    (if exit-message
+      (cli/exit (if ok? 0 1) exit-message)
+      (case action
+        "add"  (add-tasks options)
+        "list" (list-tasks options)
+        "edit" (edit-tasks options)
+        "delete" (delete-tasks options)))))
