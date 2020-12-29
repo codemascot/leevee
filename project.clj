c4194aa834d74f98526e42ced7e13dc666f43908
Khan Mohammad R <codemascot@hotmail.com>
2020-12-29 10:15:41 +0600

    URL fix in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject leevee "0.1.0-SNAPSHOT"
   :description "A todo list CLI app based on Ivy Lee method, written in Clojure."
-  :url "http://example.com/FIXME"
+  :url "https://www.codemascot.com/leevee"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
